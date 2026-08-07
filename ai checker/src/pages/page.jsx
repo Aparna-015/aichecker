ee454ab50ffd64693da967a2aa29ba3560492383
Aparna-015 <aparnaraman995@gmail.com>
2026-08-07 11:10:41 +0530

    Updated project files

---
+++ ai checker/src/pages/page.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function page() {
+
+  return (
+    <div>
+      
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </div>
+  )
+}
+
+export default page
